1322fa89d0c32db3747dbe2744ccd301c95ece34
Jens Claes <jensclaes33@gmail.com>
2016-11-05 12:26:52 +0100

    Allow disjunctions in --->

--- src/dcg_tree_expansion.pl
+++ src/dcg_tree_expansion.pl
@@ -3,33 +3,57 @@
           ]).
 :- op(1200, xfx, (--->)).
 
-to_yprime((Y, Ys), (Yprime, Yprimes), Ns2) :-
+to_yprime((Y, Ys), Yprimes3, Ns3) :-
     !,
-    to_yprime(Y, Yprime, N),
-    to_yprime(Ys, Yprimes, Ns),
-    append(N, Ns, Ns2).
-to_yprime((Y; Ys), (Yprime; Yprimes), N) :-
+    to_yprime(Y, Yprimes1, Ns1),
+    to_yprime(Ys, Yprimes2, Ns2),
+    combine(Yprimes1, Yprimes2, Yprimes3, and),
+    combine(Ns1, Ns2, Ns3, list).
+to_yprime((Y; Ys), Yprimes2, Ns3) :-
     !,
-    to_yprime(Y, Yprime, N),
-    to_yprime(Ys, Yprimes, _),
-    write(' !!! using ; in DCG\' is discouraged').
-to_yprime(L, L, [L]) :-
+    to_yprime(Y, Yprime, Ns1),
+    to_yprime(Ys, Yprimes, Ns2),
+    append(Yprime, Yprimes, Yprimes2),
+    append(Ns1, Ns2, Ns3).
+to_yprime(L, [L], [L]) :-
     L = [],
     !.
-to_yprime(L, L, [L]) :-
+to_yprime(L, [L], [L]) :-
     L = [_|_],
     !.
-to_yprime(L, L, []) :-
+to_yprime(L, [L], [[]]) :-
     L =.. ['{}' | _],
     !.
-to_yprime(Y, Yprime, [N]) :-
+to_yprime(Y, [Yprime], [[N]]) :-
     callable(Y),
     !,
     Y =.. [Functor | Arg],
     Yprime =.. [Functor, N | Arg].
-to_yprime(Y, Y, nil).
-user:term_expansion(X ---> Y, (Head --> Yprime)) :-
+to_yprime(Y, [Y], nil).
+
+user:term_expansion(X ---> Y, Rules) :-
     X =.. [Functor | Arg],
-    to_yprime(Y, Yprime, N),
+    to_yprime(Y, Yprimes, Ns),
+    maplist(to_term(Functor, Arg), Yprimes, Ns, Terms),
+    % expand DCG rules
+    maplist(expand_term, Terms, Rules).
+
+to_term(Functor, Arg, Yprime, N, (Head --> Yprime)) :-
     Capture =.. [Functor | N],
     Head =.. [Functor, Capture | Arg].
+
+combine([], _, [], _) :- !.
+combine(_, [], [], _) :- !.
+combine([X | Xs], Ys, Zs, F) :-
+    combine1(X, Ys, Zs1, F),
+    combine(Xs, Ys, Zs2, F),
+    append(Zs1, Zs2, Zs).
+
+combine1(_, [], [], _).
+combine1(X, [Y | Ys], [Z | Zs], F) :-
+    combine2(X, Y, Z, F),
+    combine1(X, Ys, Zs, F).
+
+combine2(X, Y, (X, Y), and).
+combine2(X, Y, Z, list) :-
+    append(X, Y, Z).
