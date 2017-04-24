2765d3be89d2fe616db9b9c9d34b8b3a221ba69d
Jens Claes <jensclaes33@gmail.com>
2017-04-24 14:37:47 +0200

    Fix for PNs in plural

--- bos/myGrammar.pl
+++ bos/myGrammar.pl
@@ -209,8 +209,8 @@ np([coord:yes, num:Num, gap:G, ref:no, sem:NP, vType:Type])-->
     { addTypeAttribute(Type, countable) },
     { combine(np:NP, [np:NP1, comp:Comp, np:NP2, vType1:Type, vType2:Type2]) }.
 
-np([coord:no, num:_, gap:[], ref:no, sem:NP, vType:Type])-->
-    pn([sem:PN, vType:Type]),
+np([coord:no, num:Num, gap:[], ref:no, sem:NP, vType:Type])-->
+    pn([num:Num, sem:PN, vType:Type]),
     { combine(np:NP, [pn:PN]) }.
 
 % TODO: re-add?
@@ -432,8 +432,8 @@ number([sem:Sem, vType:Type], [Number|T], T) :-
     integer(Number),
     semLex(number, [number:Number, sem:Sem, vType:Type]).
 
-pn([sem:Sem, vType:Type])-->
-    { lexEntry(pn, [symbol:Sym, syntax:Word, vType:Type]) },
+pn([num:Num, sem:Sem, vType:Type])-->
+    { lexEntry(pn, [symbol:Sym, syntax:Word, num:Num, vType:Type]) },
     optional([the]),
     Word,
     { semLex(pn, [symbol:Sym, sem:Sem]) }.
